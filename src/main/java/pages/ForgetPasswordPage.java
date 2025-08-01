b141ef3e8802a50f0d7759b9ae3cc15bcb063e2a
Dhruv Aggarwal <dhruv.aggarwal@gspann.com>
2025-08-01 22:45:22 +0530

    Added password files and step scripts

--- src/main/java/pages/ForgetPasswordPage.java
+++ src/main/java/pages/ForgetPasswordPage.java
@@ -1,18 +1,50 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import factory.DriverFactory;
 
 public class ForgetPasswordPage {
-	private final By FORGET_PASSWORD_BTN = By.linkText("Forgot your password?");
-	private final By EMAIL_FIELD = By.id("recover-email");
+
+	private final By FORGET_PASSWORD_BTN = By.xpath("//p//following::a[contains(text(),'Forgot your password?')]");
+	private final By EMAIL_FIELD = By.xpath("//input[@id='recover-email']");
+	private final By SUBMIT_BTN = By.xpath("//div[@class='action_bottom']//following::input[@value='Sign In']");
+	private final By GET_BACK_TO_HOMEPAGE_BTN = By.linkText("Login");
+	private final By FORGET_PASSWORD_TITLE = By.xpath("//h1[@class='accounts-title border bottom']");
+	private final By VERIFY_TEXT = By.xpath("//p[contains(text(),'We will send you an email to reset your password.')]");
 	
 	public void clickForgetPasswordBtn() {
-		DriverFactory.getDriver().findElement(FORGET_PASSWORD_BTN);
+		WebDriverWait wait = new WebDriverWait(DriverFactory.getDriver(), Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.elementToBeClickable(FORGET_PASSWORD_BTN)).click();
+//		DriverFactory.getDriver().findElement(FORGET_PASSWORD_BTN).click();
 	}
 	
-	public void emailField() {
-		DriverFactory.getDriver().findElement(EMAIL_FIELD);
+	public void enterEmail(String email) {
+		DriverFactory.getDriver().findElement(EMAIL_FIELD).sendKeys(email);
+	}
+	
+	public void clickSubmitBtn() {
+		DriverFactory.getDriver().findElement(SUBMIT_BTN).click();
+	}
+	
+	public void clickGetBackToHomePageBtn() {
+		DriverFactory.getDriver().findElement(GET_BACK_TO_HOMEPAGE_BTN).click();
+	}
+	
+	public By checkForgetPageTitle() {
+		return FORGET_PASSWORD_TITLE;
+	}
+
+	public void clearEmailField() {
+	    DriverFactory.getDriver().findElement(EMAIL_FIELD).clear();
+	}
+	
+	public void verifyText() {
+		DriverFactory.getDriver().findElement(VERIFY_TEXT).isDisplayed();
+		
 	}
 }
--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -1,20 +1,13 @@
 package stepdefinitions;
 
-import java.time.Duration;
-
-import org.openqa.selenium.chrome.ChromeOptions;
-
 import factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks {
-	@Before
+    @Before
     public void setup() {
         DriverFactory.getDriver().manage().window().maximize();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        DriverFactory.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
     @After
