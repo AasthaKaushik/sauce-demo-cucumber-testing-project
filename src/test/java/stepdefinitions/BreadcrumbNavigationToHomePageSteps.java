9e9b16f30b5ff36fae7a7b39e6dfc856d7de3df8
AasthaKaushik <aastha.kaushik@gspann.com>
2025-08-03 12:33:37 +0530

    Resolved Conflicts from Dhruv Branch and added login file and forget password

---
+++ src/test/java/stepdefinitions/BreadcrumbNavigationToHomePageSteps.java
@@ -0,0 +1,29 @@
+package stepdefinitions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import factory.DriverFactory;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BreadcrumbNavigationToHomePageSteps {
+	 WebDriverWait wait = new WebDriverWait(DriverFactory.getDriver(), java.time.Duration.ofSeconds(10));
+	   @Given("I am on the About Us page")
+	   public void i_am_on_the_about_us_page() {
+	       DriverFactory.getDriver().get("https://sauce-demo.myshopify.com/pages/about-us");
+
+	   }
+	   @When("I click on the Home in the breadcrumb")
+	   public void i_click_on_the_home_in_the_breadcrumb() {
+	       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@itemprop='title']"))).click();
+	   }
+	   @Then("I should be redirected to the Home page")
+	   public void i_should_be_redirected_to_the_home_page() {
+	       wait.until(ExpectedConditions.urlToBe("https://sauce-demo.myshopify.com/")); // Replace with actual home URL
+	       String currentUrl = DriverFactory.getDriver().getCurrentUrl();
+	       Assert.assertEquals("https://sauce-demo.myshopify.com/", currentUrl);
+	   }
+}
--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -1,5 +1,9 @@
 package stepdefinitions;
 
+import java.time.Duration;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -8,6 +12,9 @@ public class Hooks {
     @Before
     public void setup() {
         DriverFactory.getDriver().manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        DriverFactory.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
     @After
