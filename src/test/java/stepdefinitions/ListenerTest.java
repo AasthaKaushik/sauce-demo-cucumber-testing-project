76edf97674bac9e2afc3bf4e5b0322546d6358d7
AasthaKaushik <aastha.kaushik@gspann.com>
2025-08-05 13:07:48 +0530

    Added testng.xml to run ListenerTest

--- src/test/java/stepdefinitions/ListenerTest.java
+++ src/test/java/stepdefinitions/ListenerTest.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class ListenerTest implements ITestListener {
     @Override
     public void onTestFailure(ITestResult result) {
-        Object currentClass = result.getInstance();
+//        Object currentClass = result.getInstance();
         WebDriver driver= DriverFactory.getDriver();
         String methodName = result.getMethod().getMethodName();
         File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
